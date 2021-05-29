302857c2680cf3842a946a08ee486e7b7dff4f59
Winfidonarleyan <dowlandtop@yandex.com>
2021-05-29 22:56:29 +0700

    Init module

---
+++ src/LowLevelArena.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2016+ AzerothCore <www.azerothcore.org>, released under GNU AGPL v3 license: https://github.com/azerothcore/azerothcore-wotlk/blob/master/LICENSE-AGPL3
+ * Copyright (C) 2021+ WarheadCore <https://github.com/WarheadCore>
+ */
+
+#ifndef _LOW_LEVEL_ARENA_H_
+#define _LOW_LEVEL_ARENA_H_
+
+#include "Common.h"
+#include "SharedDefines.h"
+
+class Battleground;
+class Player;
+
+class LLA
+{
+    LLA() = default;
+    ~LLA() = default;
+
+public:
+    static LLA* instance();
+
+    void Reward(Battleground* bg, TeamId winnerTeamId);
+    void LoadConfig();
+    void AddQueue(Player* leader, uint8 arenaType, bool joinAsGroup);
+
+    LLA(LLA const&) = delete;
+    LLA(LLA&&) = delete;
+    LLA& operator= (LLA const&) = delete;
+    LLA& operator= (LLA&&) = delete;
+
+private:
+};
+
+#define sLLA LLA::instance()
+
+#endif /* _LOW_LEVEL_ARENA_H_ */
---
+++ src/low_level_arena_loader.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2016+ AzerothCore <www.azerothcore.org>, released under GNU AGPL v3 license: https://github.com/azerothcore/azerothcore-wotlk/blob/master/LICENSE-AGPL3
+ * Copyright (C) 2021+ WarheadCore <https://github.com/WarheadCore>
+ */
+
+#ifndef _LOW_LEVEL_ARENA_LOADER_H_
+#define _LOW_LEVEL_ARENA_LOADER_H_
+
+// From SC
+void AddSC_LowLevelArena();
+
+// Old api for AC
+void AddLowLevelArenaScripts()
+{
+    AddSC_LowLevelArena();
+}
+
+// New api for WH
+// void Addmod_low_level_arenaScripts()
+// {
+//     AddSC_LowLevelArena();
+// }
+
+#endif /* _LOW_LEVEL_ARENA_LOADER_H_ */
