7dd6343d5a7ee6fed25ac159d6376dc713c2515f
Francesco Borzì <borzifrancesco@gmail.com>
2021-06-20 14:09:37 +0200

    fix(SQL): update to new implementation

--- data/sql/db-world/2021_06_17_00_commands.sql
+++ data/sql/db-world/2021_06_17_00_commands.sql
@@ -1,5 +1,4 @@
 -- Added help for commands
-DELETE FROM `command` WHERE `name` IN ('lla queue solo', 'lla queue group');
+DELETE FROM `command` WHERE `name` IN ('lla queue');
 INSERT INTO `command` (`name`, `security`, `help`) VALUES
-('lla queue solo', 0, 'Syntax: .lla queue solo\r\n\r\nEnter in bg queue (arena skirmish) without group'),
-('lla queue group', 0, 'Syntax: .lla queue group\r\n\r\nEnter in bg queue (arena skirmish) with group');
+('lla queue', 0, 'Syntax: .lla queue\r\n\r\Queue Arena skirmish');
